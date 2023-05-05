84eabfa8657e6c413a3c15057862fd21e6047c22
ermil <S1098720@studenti.univpm.it>
2023-05-05 23:14:11 +0200

    Commit

--- Magazzino/Prodotto.py
+++ Magazzino/Prodotto.py
@@ -1,3 +1,5 @@
+import pickle
+import os
 
 
 class Prodotto:
@@ -12,3 +14,15 @@ class Prodotto:
 
     def get_prezzo(self):
         return self.prezzo
+
+    #ricordare:aggiungere return VistaProdotto(con le informazioni del prodotto)
+    def ricerca_prodotto_per_nome(self, nome):
+        if os.path.isfile('Dati/lista_prodotti_salvati.pickle'):
+            with open('Dati/lista_prodotti_salvati', 'rb') as f:
+                bottiglie = dict(pickle.load(f))
+                for bottiglie in bottiglie.values(): # tutti i valori nel dict
+                    if bottiglie.nome == nome:
+                        return bottiglie
+                return None
+        else:
+            return None
