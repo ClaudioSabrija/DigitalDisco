640c8aa4e728b2e442d788adcdc33f19620bef2f
claud <S1095130@studenti.univpm.it>
2023-05-16 16:19:18 +0200

    Commit

--- Viste/VistaInserisciEvento.py
+++ Viste/VistaInserisciEvento.py
@@ -59,9 +59,11 @@ class VistaInserisciEvento(QWidget):
 
         self.setLayout(layout)
 
+
+
     def add_evento(self):
-        nome = self.info["Nome*"].text()
-        cognome = self.info["Cognome*"].text()
+        nome = self.info["NOME"].text()
+        cognome = self.info["DATA"].text()
         data_nascita = self.info["Data di nascita (dd/mm/YYYY)*"].text()
         cf = self.info["Codice Fiscale*"].text()
         indirizzo = self.info["Indirizzo*"].text()
