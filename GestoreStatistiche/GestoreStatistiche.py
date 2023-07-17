4856b3c53c06f2dba03aae4e5db2fa1809396ff8
claud <S1095130@studenti.univpm.it>
2023-07-17 11:28:37 +0200

    Progetto DigitalDisco

--- GestoreStatistiche/GestoreStatistiche.py
+++ GestoreStatistiche/GestoreStatistiche.py
@@ -1,16 +1,27 @@
+from GestoreOrdini.Controller.GestoreOrdini import GestoreOrdini
+from GestoreEventi.Controller.GestoreEventi import GestoreEventi
+from datetime import datetime
 
 class GestoreStatistiche():
     def __init__(self):
+        self.controller_eventi = GestoreEventi()
+
+    def calcola_incassi_mensili_biglietti(self, mese_scelto):
+        totale_incassi = 0
+        for evento in self.controller_eventi.lista_eventi:
+            giorno, mese, anno = map(int, evento.data.split('/'))
+            data_evento = datetime(anno, mese, giorno)
+            if data_evento.month == mese_scelto:
+                for prenotazione in evento.lista_prenotazioni:
+                    totale_incassi += prenotazione.servizio.prezzo
+
+        return totale_incassi
+
+    def calcola_incassi_annuali_biglietti(self):
         pass
 
-    def calcola_incassi_ordini(self):
+    def calcola_incassi_mensili_ordini(self):
         pass
 
-    def calcola_incassi_biglietti(self):
-        pass
-
-    def calcola_incassi_mensili(self):
-        pass
-
-    def calcola_incassi_annuali(self):
+    def calcola_incassi_annuali_ordini(self):
         pass
--- GestoreStatistiche/VistaStatistiche.py
+++ GestoreStatistiche/VistaStatistiche.py
@@ -1,6 +1,56 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QComboBox, QWidget
+from PyQt5.QtChart import QChart, QChartView, QPieSeries
+from PyQt5.QtCore import Qt
+from GestoreStatistiche.GestoreStatistiche import GestoreStatistiche
 
-
-class VistaStatistiche(QMainWindow):
+class VistaStatistiche(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle("Ticket Revenue")
+        self.resize(700, 500)
+
+        self.combo_box = QComboBox()
+        self.combo_box.currentIndexChanged.connect(self.update_chart)
+
+        self.chart = QChart()
+        self.chart.legend().setVisible(True)
+        self.chart.legend().setAlignment(Qt.AlignRight)
+
+        self.chart_view = QChartView(self.chart)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.combo_box)
+        layout.addWidget(self.chart_view)
+
+        self.gestore_statistiche = GestoreStatistiche()
+
+        self.populate_combo_box()
+        self.update_chart()
+
+    def populate_combo_box(self):
+        for month in range(1, 13):
+            self.combo_box.addItem(self.get_month_name(month), month)
+
+    def get_month_name(self, month):
+        # Replace this with your own implementation to get the month name
+        month_names = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"]
+        return month_names[month - 1]
+
+    def update_chart(self):
+        selected_month = self.combo_box.currentData()
+
+        series = QPieSeries()
+        for month in range(1, 13):
+            month_name = self.get_month_name(month)
+            revenue = self.gestore_statistiche.calcola_incassi_mensili_biglietti(month)
+            if month == selected_month:
+                series.append(f"{month_name} (Selected)", revenue)
+            else:
+                series.append(month_name, revenue)
+
+        self.chart.removeAllSeries()
+        self.chart.addSeries(series)
+        self.chart.setTitle("Ticket Revenue by Month")
+
+        self.chart_view.repaint()
